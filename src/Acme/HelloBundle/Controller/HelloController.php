33ba7aee4985c3cd75651de2731a69798019c96e
Kate Radu <Kate@Kates-MacBook-Pro.local>
2014-04-16 12:31:57 -0700

    Added test page hello {name}

---
+++ src/Acme/HelloBundle/Controller/HelloController.php
@@ -0,0 +1,13 @@
+// src/Amce/HelloBundle/Controller/HelloController.php
+namespace Acme\HelloBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class HelloController
+{
+	public function indexAction($name)
+	{
+		return new Response('<html><body>Hello '.$name.'!</body></html>');
+	}
+}
+
